e73578182066b7d0d2b7d6588bd7077a468c05e0
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2022-05-16 11:14:13 -0700

    Aws

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 let btn = document.getElementById("close");
 let awsBanner = document.getElementById("AWS");
-btn.addEventListener("click", ()=>awsBanner.style.display = "none");
+btn.addEventListener("click", (e)=>{
+awsBanner.style.display = "none";
+e.stopPropagation();
+});
+awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
