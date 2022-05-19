848b0a0331b12fbc6997455bb3b7a6dc4b91f511
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2022-05-19 15:37:48 -0700

    Added Home and Nav Bar

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let btn = document.getElementById("close");
-let awsBanner = document.getElementById("AWS");
-btn.addEventListener("click", (e)=>{
-awsBanner.style.display = "none";
-e.stopPropagation();
-});
-awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
+// let btn = document.getElementById("close");
+// let awsBanner = document.getElementById("AWS");
+
+// let nav2 = document.getElementsByClassName("nav-item");
+// btn.addEventListener("click", (e)=>{
+// awsBanner.style.display = "none";
+// e.stopPropagation();
+// });
+// awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
+// hometab.addEventListener("click", ()=>{
+
+function scrolltop(){
+    document.getElementById("navtop").scrollIntoView();
+}
