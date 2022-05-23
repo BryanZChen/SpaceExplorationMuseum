6119e750c8fbc669e480ea452f22ba7098116c0f
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2022-05-23 11:19:16 -0700

    moved time line and styled

--- app.js
+++ app.js
@@ -9,57 +9,55 @@
 // awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
 // hometab.addEventListener("click", ()=>{
 
-function scrolltop(){
-    document.getElementById("navtop").scrollIntoView();
+function scrolltop() {
+  document.getElementById("navtop").scrollIntoView();
 }
-$(function(){
+$(function () {
 
-    window.sr = ScrollReveal();
-  
-    if ($(window).width() < 768) {
-  
-      if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-        $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-      }
-  
-      sr.reveal('.js--fadeInRight', {
-        origin: 'right',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-      });
-  
-    } else {
-      
-      sr.reveal('.js--fadeInLeft', {
-        origin: 'left',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-      });
-  
-      sr.reveal('.js--fadeInRight', {
-        origin: 'right',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-      });
-  
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
     }
-    
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+
     sr.reveal('.js--fadeInLeft', {
-        origin: 'left',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-      });
-  
-      sr.reveal('.js--fadeInRight', {
-        origin: 'right',
-        distance: '300px',
-        easing: 'ease-in-out',
-        duration: 800,
-      });
-  
-  
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+
+  sr.reveal('.js--fadeInLeft', {
+    origin: 'left',
+    distance: '300px',
+    easing: 'ease-in-out',
+    duration: 800,
   });
+
+  sr.reveal('.js--fadeInRight', {
+    origin: 'right',
+    distance: '300px',
+    easing: 'ease-in-out',
+    duration: 800,
+  });
+});
