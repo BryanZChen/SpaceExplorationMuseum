5f0ea5bca9cefbdcc7587239dd2ef0af51a59bb2
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2022-05-25 14:18:55 -0700

    New Time line look

--- app.js
+++ app.js
@@ -8,56 +8,126 @@
 // });
 // awsBanner.addEventListener("click", ()=>location.href = "https://www.amazon.com/");
 // hometab.addEventListener("click", ()=>{
-
-function scrolltop() {
-  document.getElementById("navtop").scrollIntoView();
-}
-$(function () {
-
-  window.sr = ScrollReveal();
-
-  if ($(window).width() < 768) {
-
-    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-    }
-
-    sr.reveal('.js--fadeInRight', {
-      origin: 'right',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
+  (function ($) {
+    $(function () {
+  
+  
+      $(window).on('scroll', function () {
+        fnOnScroll();
+      });
+  
+      $(window).on('resize', function () {
+        fnOnResize();
+      });
+  
+  
+      var agTimeline = $('.js-timeline'),
+        agTimelineLine = $('.js-timeline_line'),
+        agTimelineLineProgress = $('.js-timeline_line-progress'),
+        agTimelinePoint = $('.js-timeline-card_point-box'),
+        agTimelineItem = $('.js-timeline_item'),
+        agOuterHeight = $(window).outerHeight(),
+        agHeight = $(window).height(),
+        f = -1,
+        agFlag = false;
+  
+      function fnOnScroll() {
+        agPosY = $(window).scrollTop();
+  
+        fnUpdateFrame();
+      }
+  
+      function fnOnResize() {
+        agPosY = $(window).scrollTop();
+        agHeight = $(window).height();
+  
+        fnUpdateFrame();
+      }
+  
+      function fnUpdateWindow() {
+        agFlag = false;
+  
+        agTimelineLine.css({
+          top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
+          bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
+        });
+  
+        f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
+      }
+  
+      function fnUpdateProgress() {
+        var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
+  
+        i = agTop + agPosY - $(window).scrollTop();
+        a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+        n = agPosY - a + agOuterHeight / 2;
+        i <= agPosY + agOuterHeight / 2 && (n = i - a);
+        agTimelineLineProgress.css({height: n + "px"});
+  
+        agTimelineItem.each(function () {
+          var agTop = $(this).find(agTimelinePoint).offset().top;
+  
+          (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
+        })
+      }
+  
+      function fnUpdateFrame() {
+        agFlag || requestAnimationFrame(fnUpdateWindow);
+        agFlag = true;
+      }
+  
+  
     });
+  })(jQuery);
+  // function scrolltop() {
+  //   document.getElementById("navtop").scrollIntoView();
+  // }
+  // $(function () {
 
-  } else {
+  //   window.sr = ScrollReveal();
 
-    sr.reveal('.js--fadeInLeft', {
-      origin: 'left',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
+  //   if ($(window).width() < 768) {
 
-    sr.reveal('.js--fadeInRight', {
-      origin: 'right',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
+  //     if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+  //       $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  //     }
 
-  }
+  //     sr.reveal('.js--fadeInRight', {
+  //       origin: 'right',
+  //       distance: '300px',
+  //       easing: 'ease-in-out',
+  //       duration: 800,
+  //     });
 
-  sr.reveal('.js--fadeInLeft', {
-    origin: 'left',
-    distance: '300px',
-    easing: 'ease-in-out',
-    duration: 800,
-  });
+  //   } else {
 
-  sr.reveal('.js--fadeInRight', {
-    origin: 'right',
-    distance: '300px',
-    easing: 'ease-in-out',
-    duration: 800,
-  });
-});
+  //     sr.reveal('.js--fadeInLeft', {
+  //       origin: 'left',
+  //       distance: '300px',
+  //       easing: 'ease-in-out',
+  //       duration: 800,
+  //     });
+
+  //     sr.reveal('.js--fadeInRight', {
+  //       origin: 'right',
+  //       distance: '300px',
+  //       easing: 'ease-in-out',
+  //       duration: 800,
+  //     });
+
+  //   }
+
+  //   sr.reveal('.js--fadeInLeft', {
+  //     origin: 'left',
+  //     distance: '300px',
+  //     easing: 'ease-in-out',
+  //     duration: 800,
+  //   });
+
+  //   sr.reveal('.js--fadeInRight', {
+  //     origin: 'right',
+  //     distance: '300px',
+  //     easing: 'ease-in-out',
+  //     duration: 800,
+  //   });
+  // });
